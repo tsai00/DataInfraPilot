8561b7358a647451835398150b4c953f900ab151
Ilya Tsakunov <ilyatsakunov@seznam.cz>
2025-03-20 21:22:27 +0100

    Add config with constants

---
+++ src/core/config.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv(Path(Path(__file__).parent.parent.parent.absolute(), '.env'))
+
+PATH_TO_K3S_YAML_CONFIGS = Path(Path(__file__).absolute().parent.parent.parent, 'output')
+
+PATH_TO_K3S_YAML_CONFIGS.mkdir(exist_ok=True)
+
+HCLOUD_TOKEN = os.environ.get("HCLOUD_TOKEN")
+K3S_TOKEN = os.environ.get("K3S_TOKEN")
+SSH_PASSWORD = os.environ.get("SSH_PASSWORD")
