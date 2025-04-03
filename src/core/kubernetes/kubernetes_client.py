864e81cb9f2a658daf5698de690b740c24ddd36c
Ilya Tsakunov <ilyatsakunov@seznam.cz>
2025-04-03 20:29:29 +0200

    Add method to apply custom objects

--- src/core/kubernetes/kubernetes_client.py
+++ src/core/kubernetes/kubernetes_client.py
@@ -1,4 +1,6 @@
 from kubernetes import client, config, utils
+from kubernetes.dynamic.client import DynamicClient
+from kubernetes.dynamic.exceptions import NotFoundError
 from pathlib import Path
 import yaml
 
@@ -13,6 +15,8 @@ class KubernetesClients:
         self.RBAC = client.RbacAuthorizationV1Api()  # Role-Based Access Control
         self.CustomObjects = client.CustomObjectsApi()  # Custom Resources (CRDs)
 
+        self.Dynamic = DynamicClient(client.ApiClient())
+
 
 class KubernetesClient:
     def __init__(self, kubeconfig_path: Path):
@@ -20,8 +24,30 @@ class KubernetesClient:
 
         self._clients = KubernetesClients()
 
-    def install_from_yaml(self, path_to_yaml: Path):
+    def install_from_yaml(self, path_to_yaml: Path, with_custom_objects: bool = False):
         if path_to_yaml.is_dir():
-            utils.create_from_directory(self._clients.Api, str(path_to_yaml))
+            utils.create_from_directory(self._clients.CustomObjects, str(path_to_yaml))
         else:
-            utils.create_from_yaml(self._clients.Api, str(path_to_yaml))
+            manifest = yaml.safe_load(path_to_yaml.read_text())
+            if with_custom_objects:
+                print('Applying custom objects')
+                self._apply_simple_item(manifest)
+            else:
+                utils.create_from_yaml(self._clients.CustomObjects, str(path_to_yaml))
+
+    def _apply_simple_item(self, manifest: dict, verbose: bool = False):
+        api_version = manifest.get("apiVersion")
+        kind = manifest.get("kind")
+        resource_name = manifest.get("metadata").get("name")
+        namespace = manifest.get("metadata").get("namespace")
+        crd_api = self._clients.Dynamic.resources.get(api_version=api_version, kind=kind)
+
+        try:
+            crd_api.get(namespace=namespace, name=resource_name)
+            crd_api.patch(body=manifest, content_type="application/merge-patch+json")
+            if verbose:
+                print(f"{namespace}/{resource_name} patched")
+        except NotFoundError:
+            crd_api.create(body=manifest, namespace=namespace)
+            if verbose:
+                print(f"{namespace}/{resource_name} created")
