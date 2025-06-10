e2432d71d33b01970c33f25f14e4ea59e33d60ec
Ilya Tsakunov <ilyatsakunov@seznam.cz>
2025-06-10 21:18:18 +0200

    Implement sreality and bezrealitky transformations for demo project

---
+++ src/demo/scrapers/bezrealitky/transformation.py
@@ -0,0 +1,116 @@
+import re
+
+import pandas as pd
+from src.demo.scrapers.base_transformation import BaseTransformation, TransformationError
+
+
+class BezrealitkyTransformation(BaseTransformation):
+    def __init__(self):
+        self._known_disposition_types = {
+            'DISP_3_1': '3+1',
+            'DISP_2_KK': '2+kk',
+            'DISP_3_KK': '3+kk',
+            'DISP_2_1': '2+1',
+            'DISP_1_KK': '1+kk',
+            'UNDEFINED': 'undefined',
+            'DISP_1_1': '1+1',
+            'DISP_2_IZB': '2',
+            'GARSONIERA': 'garsoniera',
+            'DISP_4_1': '4+1',
+            'DISP_4_KK': '4+kk',
+            'DISP_6_1': '6+1',
+            'DISP_1_IZB': '1',
+            'DISP_3_IZB': '3',
+            'DISP_5_1': '5',
+            'DISP_4_IZB': '4',
+            'OSTATNI': 'other',
+            'DISP_5_KK': '5+kk',
+            'DISP_7_1': '7+1',
+            'DISP_6_KK': '6+kk',
+            'DISP_7_KK': '7+kk'
+        }
+
+        super().__init__('BezrealitkyTransformation')
+
+    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
+        cols_to_select = [
+            'id',
+            'uri',
+            'disposition',
+            'imageAltText',
+            'address',
+            'surface',
+            'price',
+            'currency',
+            'gps',
+            '_scraped_at'
+        ]
+
+        cols_to_drop = [
+            'gps',
+            'currency',
+            'address'
+        ]
+
+        map_old_new_names = {
+            'id': 'internal_id',
+            'uri': 'url',
+            'imageAltText': 'name',
+            'surface': 'area',
+        }
+
+        df_transformed = (
+            df
+            .pipe(self.select_columns, cols_to_select)
+            .pipe(self.map_values_with_validation, 'disposition', self._known_disposition_types)
+            .pipe(self.filter_data)
+            .pipe(self.add_new_columns)
+            .pipe(self.rename_columns, map_old_new_names)
+            .pipe(self.drop_columns, cols_to_drop)
+        )
+
+        return df_transformed
+
+    @staticmethod
+    def _parse_address(row: dict):
+        # Note: the following parsing function is only intended for Czech addresses
+        # (~ apply after currency filtering)
+        address = row['address']
+        address_elements = re.split(r", | - ", address)
+
+        if len(address_elements) == 2:
+            city, city_part = address_elements
+            street, region = None, None
+        elif len(address_elements) == 3:
+            street, city, city_part = address_elements
+
+            region = 'Hlavní město Praha' if city == 'Praha' else None
+        elif len(address_elements) == 4:
+            street, city, city_part, region = address_elements
+        else:
+            raise TransformationError(f"Unknown value type for address: {address}")
+
+        return region, city, city_part, street
+
+    def add_new_columns(self, df: pd.DataFrame) -> pd.DataFrame:
+        df_copy = df.copy()
+
+        df_copy['uri'] = 'https://www.bezrealitky.cz/nemovitosti-byty-domy/' + df_copy['uri'].astype(str)
+
+        df_copy[['region', 'city', 'city_part', 'street']] = df_copy.apply(self._parse_address, axis=1, result_type='expand')
+
+        df_copy['lat'] = df_copy['gps'].apply(self.get_dict_element, dict_key='lat').astype(float)
+        df_copy['lon'] = df_copy['gps'].apply(self.get_dict_element, dict_key='lng').astype(float)
+
+        return df_copy
+
+    def filter_data(self, df: pd.DataFrame) -> pd.DataFrame:
+        self._logger.info(f'Number of records before filtering: {len(df)}')
+
+        df_filtered = df[
+            (df['currency'] == 'CZK')
+        ]
+
+        self._logger.info(f'Number of records after filtering: {len(df_filtered)}')
+
+        return df_filtered
---
+++ src/demo/scrapers/sreality/transformation.py
@@ -0,0 +1,104 @@
+import numpy as np
+import pandas as pd
+from src.demo.scrapers.base_transformation import BaseTransformation
+
+
+class SrealityTransformation(BaseTransformation):
+    def __init__(self):
+        super().__init__('SrealityTransformation')
+
+    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
+        cols_to_select = [
+            'name',
+            'id',
+            'categorySubCb',
+            'categoryTypeCb',
+            'locality',
+            'priceCzk',
+            'priceUnitCb',
+            '_scraped_at'
+        ]
+
+        cols_to_explode = ['locality']
+        cols_to_drop = [
+            'categorySubCb', 'priceUnitCb', 'locality.citySeoName', 'locality.cityPartSeoName',
+            'locality.country', 'locality.countryId', 'locality.districtId', 'locality.districtSeoName',
+            'locality.entityType', 'locality.geoHash', 'locality.inaccuracyType', 'locality.municipality',
+            'locality.municipalityId', 'locality.municipalitySeoName', 'locality.quarter', 'locality.quarterId',
+            'locality.regionId', 'locality.regionSeoName', 'locality.streetId', 'locality.streetSeoName',
+            'locality.ward', 'locality.wardId', 'locality.wardSeoName', 'locality.zip', 'categoryTypeCb',
+            'locality.streetNumber', 'locality.houseNumber', 'locality.district'
+        ]
+
+        map_old_new_names = {
+            'id': 'internal_id',
+            'priceCzk': 'price',
+            'locality.city': 'city',
+            'locality.cityPart': 'city_part',
+            'locality.latitude': 'lat',
+            'locality.longitude': 'lon',
+            'locality.region': 'region',
+            'locality.street': 'street',
+        }
+
+        df_transformed = (
+            df
+            .pipe(self.select_columns, cols_to_select)
+            .pipe(self.convert_columns)
+            .pipe(self.explode_columns, cols_to_explode)
+            .pipe(self.add_new_columns)
+            .pipe(self.filter_data)
+            .pipe(self.drop_columns, cols_to_drop)
+            .pipe(self.rename_columns, map_old_new_names)
+        )
+
+        return df_transformed
+
+    def convert_columns(self, df: pd.DataFrame) -> pd.DataFrame:
+        df_copy = df.copy()
+
+        df_copy['locality'] = df_copy['locality'].apply(self.get_dict_element)
+
+        return df_copy
+
+    def _construct_url(self, row: dict):
+        disposition = row['disposition']
+        city = row['locality.citySeoName']
+        district = row['locality.cityPartSeoName']
+        street = row['locality.streetSeoName']
+        internal_id = row['id']
+        category = row['categoryTypeCb']
+
+        return f'https://www.sreality.cz/detail/{category}/byt/{disposition}/{city}-{district}-{street}/{internal_id}'
+
+    def add_new_columns(self, df: pd.DataFrame) -> pd.DataFrame:
+        df_copy = df.copy()
+
+        df_copy['area'] = df_copy['name'].str.extract(r"(\d{1,}) m²").astype(int)
+
+        df_copy['priceUnitCb'] = df_copy['priceUnitCb'].apply(self.get_dict_element, dict_key='name')
+
+        df_copy['categoryTypeCb'] = df_copy['categoryTypeCb'].apply(self.get_dict_element, dict_key='name').str.lower()
+        df_copy['categoryTypeCb'] = df_copy['categoryTypeCb'].apply(self.normalize_string)
+
+        df_copy['disposition'] = df_copy['categorySubCb'].apply(self.get_dict_element, dict_key='name').str.lower()
+        df_copy['disposition'] = df_copy['disposition'].apply(self.normalize_string)
+
+        df_copy['url'] = df_copy.apply(lambda row: self._construct_url(row), axis=1)
+
+        df_copy['priceCzk'] = np.where(df_copy['priceUnitCb'] == 'za m²', df_copy['priceCzk'] * df_copy['area'], df_copy['priceCzk'])
+
+        return df_copy
+
+    def filter_data(self, df: pd.DataFrame) -> pd.DataFrame:
+        self._logger.info(f'Number of records before filtering: {len(df)}')
+
+        price_period_exclude = ['za měsíc'] if df['categoryTypeCb'].tolist()[0] == 'prodej' else []
+
+        df_filtered = df[
+            (~df['priceUnitCb'].isin(price_period_exclude))
+        ]
+
+        self._logger.info(f'Number of records after filtering: {len(df_filtered)}')
+
+        return df_filtered
