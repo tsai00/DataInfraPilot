d679becdaaa66b7fc7f073a8fdda7102dcd3e6bb
Ilya Tsakunov <ilyatsakunov@seznam.cz>
2025-06-15 15:43:01 +0200

    Allow to execute script from any location

--- demo/src/orchestration/run_pipeline.sh
+++ demo/src/orchestration/run_pipeline.sh
@@ -2,10 +2,12 @@
 
 # Usage: ./run_pipeline.sh sreality rent 20250608
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 if [ -z "$1" ]
   then
     echo 'No project name provided. Choose one from [sreality, bezrealitky]'
-    exit 0
+    exit 1
 else
   project=$1
 fi
@@ -14,7 +16,7 @@ fi
 if [ -z "$2" ]
   then
     echo 'No listing_type provided. Choose one from [rent, sale]'
-    exit 0
+    exit 1
 else
   listing_type=$2
 fi
@@ -28,6 +30,17 @@ else
   batchid=$3
 fi
 
-python3 scrape.py --project $project --listing-type $listing_type --batch-id $batchid
-python3 transform.py --project $project --listing-type $listing_type --batch-id $batchid
-python3 upload_to_db.py --project $project --listing-type $listing_type --batch-id $batchid
+
+echo "Running scrape.py for project: $project, listing_type: $listing_type, batch_id: $batchid"
+python3 "$SCRIPT_DIR/scrape.py" --project "$project" --listing-type "$listing_type" --batch-id "$batchid"
+if [ $? -ne 0 ]; then echo "Error: scrape.py failed."; exit 1; fi # Check last command's exit status
+
+echo "Running transform.py..."
+python3 "$SCRIPT_DIR/transform.py" --project "$project" --listing-type "$listing_type" --batch-id "$batchid"
+if [ $? -ne 0 ]; then echo "Error: transform.py failed."; exit 1; fi
+
+echo "Running upload_to_db.py..."
+python3 "$SCRIPT_DIR/upload_to_db.py" --project "$project" --listing-type "$listing_type" --batch-id "$batchid"
+if [ $? -ne 0 ]; then echo "Error: upload_to_db.py failed."; exit 1; fi
+
+echo "Pipeline finished successfully."
