a2e3c44f806f2608378800b5c0d8209c84ae5c66
Ilya Tsakunov <ilyatsakunov@seznam.cz>
2025-06-15 21:52:24 +0200

    Add table schemas for demo project

---
+++ demo/src/storage/schemas/rent.sql
@@ -0,0 +1,17 @@
+CREATE TABLE public.rent (
+	id BIGSERIAL PRIMARY key,
+	internal_id text,
+	url text,
+	disposition text NULL,
+	"name" text NULL,
+	area int8 NULL,
+	price int8 NULL,
+	region text NULL,
+	city text NULL,
+	city_part text NULL,
+	street text NULL,
+	lat float8 NULL,
+	lon float8 NULL,
+	_scraped_at TIMESTAMP WITH TIME ZONE,
+	_data_source text
+);
